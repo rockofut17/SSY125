31f9cc9ca97596c75d949a6e69fb1951a9c6e80f
JohanEkenvi <94176491+JohanEkenvi@users.noreply.github.com>
2022-12-22 09:10:04 +0100

    Add files via upload
    Function for eps1,eps2,eps3 using conv

---
+++ Encoder.m
@@ -0,0 +1,25 @@
+function full_message = Encoder(msg,type)
+% This function encodes a bit message using convolutional encoder.
+% It takes type of encoder and message to encode as argument.
+% Note on generator Matrix, they are ordered in decending order
+% that is starting on highest polynomial to lowest
+% Returns: Encoded message
+switch type
+    case 'eps1'
+        G1 = [1 1 1];
+        G2 = [1 0 1]; 
+    case 'eps2'
+        G1 = [1 1 1 0 1];
+        G2 = [0 1 1];
+    case 'eps3'
+        G1 = [1 1 0 0 1];
+        G2 = [1 1 0 1 1];
+end
+% Encode the message using the conv function
+        encoded_msg1 = mod(conv(msg,G1),2);
+        encoded_msg2 = mod(conv(msg,G2),2);
+        full_message = [encoded_msg1(1:end-4) ; encoded_msg2(1:end-4)];
+        full_message = reshape(full_message,1,[]);
+       
+
+end
